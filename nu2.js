922395ea61a8138802e414bf53141e6346344103
dengtianci <94218985+dengtianci@users.noreply.github.com>
2021-11-16 14:05:09 +0800

    Add files via upload

---
+++ nu2.js
@@ -0,0 +1,5 @@
+document.writeln("<script language=javascript>");
+document.writeln("var number = Math.random();");
+document.writeln("number = Math.ceil(number * 30); ");
+document.writeln("document.write(number);");
+document.writeln("<\/script>")
